d7820abae8820521927defd96287fea25e42cea0
Ephie <ephie.lam@kcl.ac.uk>
2019-07-04 16:47:21 +0100

    Remove tinymce link

--- builder.php
+++ builder.php
@@ -38,6 +38,9 @@
           <?php include('components/transcript.php'); ?>
         </div>   
       </section>
+      <section style="display:none;">
+        <p><a href="http://tinymce-bootstrap-plugin.creation-site.org/">tinyMce Bootstrap Plugin</a></p>
+      </section>
     </main>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
