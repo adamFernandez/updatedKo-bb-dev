926fc8d42b2808105783d2388af0c1941948d9fe
Ephie <ephie.lam@kcl.ac.uk>
2019-10-22 09:58:24 +0100

    FAQS: add questions

--- components/faqs.php
+++ components/faqs.php
@@ -3,8 +3,9 @@
     <h2 class="component-title">FAQs</h2>
   </div>
   <div class="component-body container">
-    <div class="alert alert-info alert-nope mt-3" role="alert">
-      <p>Soz, I haven't written these just yet. Ask me a Q on Slack!</p>
-    </div> 
+    <h3>How can I report an issue?</h3>
+    <p>If something isn't working as expected, <a href="https://docs.google.com/forms/d/e/1FAIpQLScV9PqAFCSyXcnbJoGPpG_BEB3y5fMZv0TggBvHzWl41u5v1w/viewform">submit a bug report via the Google form</a>.</p>
+    <h3>Where can I find examples of all the different components?</h3>
+    <p>The Design System test course has a <a href="https://keats.kcl.ac.uk/course/view.php?id=73004&section=1">Kitchen Sink</a> that demonstrates all of the different components that can be built.</p>
   </div>
 </div>
