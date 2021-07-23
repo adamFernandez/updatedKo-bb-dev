d0659b294f7e074acb70f4e40065527353b38411
Alesito <aarodriguezfernandez@gmail.com>
2021-07-23 09:09:22 +0100

    Created timeline, form function refactored

--- components/timeline.php
+++ components/timeline.php
@@ -1,67 +1,79 @@
 <div class="component-content" id="component-timeline">
   <div class="component-header">
-    <h2 class="component-title">Timeline</h2>
-    <p><a href="https://keats.kcl.ac.uk/mod/page/view.php?id=3119704" target="_blank">Design System guidance and examples</a></p>
+    <h2 class="component-title">Process</h2>
+    <p><a href="https://keats.kcl.ac.uk/mod/page/view.php?id=3118002" target="_blank">Design System guidance and examples</a></p>
   </div>
   <div class="component-body container">
+    <div class="alert alert-danger d-none" role="alert" id="timeline-alt-text-alert">
+      <p>Please add alternative text for all images.</p>
+    </div>
     <div class="row">
       <div class="options col-md-5">
         <section class="options">
           <h3>Options</h3>
           <p>Complete the following fields:</p>
-          <form>
-            <div class="form-row">
-              <div class="form-group col-md-4">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <label class="input-group-text" for="tl-card-no">No. of cards</label>
-                  </div>
-                  <select class="custom-select" id="tl-card-no">
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4" selected>4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                  </select>
-                </div>
-              </div>
-              <!--
-              <div class="form-group col-md-8">
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <label class="input-group-text" for="tl-id">Unique ID</label>
-                  </div>
-                  <input type="text" class="form-control" id="tl-id" aria-label="tl-id" placeholder="modname-unitno-timeline-no" aria-describedby="timeline-unique-id">
-                </div>
-                <small id="tl-id-help" class="form-text text-muted">Optional ID to assist with identification and custom styling.</small>
-              </div>
-               -->
+          <form class="needs-validation" id="tl-form" novalidate>
+            <!-- <div class="custom-control custom-checkbox mb-3">
+              <input type="checkbox" class="custom-control-input" id="timeline-check-label" unchecked>
+              <label class="custom-control-label" id="timeline-label-label" for="timeline-check-label">Show All Labels</label>
             </div>
-            <div class="custom-control custom-checkbox mb-3">
-              <input type="checkbox" class="custom-control-input checked" id="tl-check-date" checked>
-              <label class="custom-control-label" for="tl-check-date"><span id="tl-toggle-date">Remove</span> dates</label>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="timeline-arrows">Choose arrow type for all cards</label>
+              </div>
+              <select class="custom-select" id="timeline-arrows">
+                  <option value="1" selected>Arrow Down</option>
+                  <option value="2">Arrow Up</option>
+                  <option value="3">Double Arrow</option>
+                  <option value="4">Relation</option>
+              </select>                
+            </div> -->
+            <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="timeline-cards">No. of cards</label>
+              </div>
+              <select class="custom-select" id="timeline-select">
+                  <option value="2">2</option>
+                  <option value="3" selected>3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+              </select>
+            </div>                
+            <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="timeline-check-img" checked>
+              <label class="custom-control-label" id="timeline-label-img" for="timeline-check-img">Remove All Images</label>
             </div>
+            <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="timeline-check-caption" checked>
+              <label class="custom-control-label" id="timeline-label-caption" for="timeline-check-caption">Remove All Caption Titles</label>
+            </div>            
           </form>
-          <div class="collapse-container" id="tl-cards">
+          <div class="collapse-container" id="timeline-collapse-container">
+          
           </div>
         </section>
       </div>
       <div class="col-md-7">
         <section class="preview">
           <h3 class="preview-title">Preview</h3>
-          <div class="card" id="tl-preview-pane"></div>
+          <div class="preview-timeline">
+            <div class="timeline-container timeline-2021-container">
+            </div>
+          </div>
         </section>
         <section class="component-code">
           <div class="code-header">
             <h3 class="code-title">Code</h3>
-            <button class="btn btn-secondary" id="copy-tl-code" type="submit">Copy Code</button>
+            <button class="btn btn-secondary" id="copy-timeline-code">Copy Code</button>
           </div>
           <div class="code-box">
-<pre class="code-text"><span id="tl-print-code"><span class="code-open-tag">&lt;div&#32;class&#61;&#34;timeline&#45;container&#34;<span id="code-tl-id"></span>&gt;</span>
-  <span id="code-tl-cards"></span>
-<span class="code-close-tag">&lt;&#47;div&gt;</span></span></pre>
+<pre class="code-text">
+&lt;div class=&quot;timeline-container timeline-2021-container&quot;&gt;
+  <span id="code-timeline"></span>&lt;/div&gt;
+</pre>
           </div>
         </section>
       </div>
